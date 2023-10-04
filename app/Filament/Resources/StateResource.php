b568a6d618f54160cbb7b6327818ae902beacb7f
An Khang <khang.nguyenan123@gmail.com>
2023-10-04 20:42:35 +0700

    filament project

---
+++ app/Filament/Resources/StateResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StateResource\Pages;
+use App\Filament\Resources\StateResource\RelationManagers;
+use App\Models\State;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StateResource extends Resource
+{
+    protected static ?string $model = State::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-library';
+
+    protected static ?string $navigationLabel = "State";
+
+    protected static ?string $modelLabel = "States";
+
+    protected static ?string $navigationGroup = "System Management";
+
+    protected static ?int $navigationSort = 2;
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('User Name')
+                    ->description('Put the user name details in.')
+                    ->schema(
+                        [
+                            Forms\Components\Select::make('state_id')
+                                ->relationship(name: 'state', titleAttribute: 'name')
+                                ->searchable(true)
+                                ->preload()
+                                ->required(),
+                        ]
+                    )->columns(2),
+
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStates::route('/'),
+            'create' => Pages\CreateState::route('/create'),
+            'edit' => Pages\EditState::route('/{record}/edit'),
+        ];
+    }
+}
